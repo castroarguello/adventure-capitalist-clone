e72a0012d13dfa98f77b333b5b008c866d4a668c
Ramiro Castro Arguello <rcastro@phase2technology.com>
2020-08-13 20:44:07 -0300

    Add bootstrap

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -3,6 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
 });
