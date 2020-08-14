0e3216755622df353a24bc0623235d7c91141170
Ramiro Castro Arguello <rcastro@phase2technology.com>
2020-08-14 18:13:36 -0300

    Update business data

--- app/server/main.js
+++ app/server/main.js
@@ -5,8 +5,8 @@ import { PlayersCollection } from '/imports/api/players';
 import { BusinessCollection } from '/imports/api/business';
 import { Random } from 'meteor/random';
 
-function insertType({ id, name, upgradeRate, profit }) {
-  TypesCollection.insert({id, name, upgradeRate, profit, createdAt: new Date()});
+function insertType({ id, name, upgradeRate, profit, duration, initialCost }) {
+  TypesCollection.insert({id, name, upgradeRate, profit, duration, initialCost, createdAt: new Date()});
 }
 
 function insertPlayer(player) {
@@ -32,9 +32,9 @@ Meteor.startup(() => {
       { id: "donut", name: "Donut Shop", upgradeRate: 1.12, profit: 51480, duration: 24, initialCost: 103680 },
       { id: "shrimp", name: "Schrimp Boat", upgradeRate: 1.11, profit: 622080, duration: 96, initialCost: 1244160 },
       { id: "hockey", name: "Hockey Team", upgradeRate: 1.1, profit: 7464000, duration: 384, initialCost: 14929920 },
-      { id: "studio", name: "Movie Studio", upgradeRate: 1.13, profit: 4320, duration: 12, initialCost: 179159040 },
-      { id: "bank", name: "Bank", upgradeRate: 1.13, profit: 4320, duration: 12, initialCost: 2149908480 },
-      { id: "oil", name: "Oil Company", upgradeRate: 1.13, profit: 4320, duration: 12, initialCost: 25798901760 },
+      { id: "studio", name: "Movie Studio", upgradeRate: 1.09, profit: 89579000, duration: 1536, initialCost: 179159040 },
+      { id: "bank", name: "Bank", upgradeRate: 1.08, profit: 4320, duration: 12, initialCost: 2149908480 },
+      { id: "oil", name: "Oil Company", upgradeRate: 1.07, profit: 4320, duration: 12, initialCost: 25798901760 },
     ].forEach(insertType);
   }
 
