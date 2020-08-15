80b6149f8abae313d07b9ffd7e3388e15a95ec7b
Ramiro Castro Arguello <rcastro@phase2technology.com>
2020-08-15 20:03:00 -0300

    Currency format 2 decimals

--- app/imports/api/business.js
+++ app/imports/api/business.js
@@ -10,22 +10,25 @@ Meteor.methods({
     const type = TypesCollection.find({ id: typeId }).fetch()[0];
     const player = PlayersCollection.find({ _id: playerId }).fetch()[0];
     const upgradeCost = type.upgradeRate * type.purchase;
-    const business = {
-      level: 1,
-      player: playerId,
-      profit: type.profit,
-      timestamp: new Date().getTime(),
-      type: typeId,
-      upgradeCost: upgradeCost.toFixed(2),
-    };
-      // Insert level 1 business.
-    BusinessCollection.insert(business);
 
     // Extract purchase cost except the lemonade stand.
-    if (typeId != 'lemonade') {
-      const cash = +(player.cash - type.purchase);
-      player.cash = +cash.toFixed(2);
-      PlayersCollection.update({ _id: playerId }, player);
+    const cash = +(player.cash - type.purchase);
+    if (cash >= 0) {
+      if (typeId != 'lemonade') {
+        player.cash = +cash.toFixed(2);
+        PlayersCollection.update({ _id: playerId }, player);
+      }
+
+      const business = {
+        level: 1,
+        player: playerId,
+        profit: type.profit,
+        timestamp: new Date().getTime(),
+        type: typeId,
+        upgradeCost: upgradeCost.toFixed(2),
+      };
+        // Insert level 1 business.
+      BusinessCollection.insert(business);
     }
   },
 
@@ -36,21 +39,22 @@ Meteor.methods({
     const business = BusinessCollection.find({ _id: businessId }).fetch()[0];
 
     if (business) {
-
       // Extract upgrade cost.
       const cash = player.cash - business.upgradeCost;
-      player.cash = +cash.toFixed(2);
-      player.updatedAt = new Date().getTime();
-      PlayersCollection.update({ _id: playerId }, player);
+      if (cash >= 0) {
+        player.cash = +cash.toFixed(2);
+        player.updatedAt = new Date().getTime();
+        PlayersCollection.update({ _id: playerId }, player);
 
-      business.level++;
-      const upgradeCost = Math.pow(type.upgradeRate, business.level) * type.purchase;
-      business.profit = type.profit * business.level;
-      business.timestamp = new Date().getTime();
-      business.upgradeCost = upgradeCost.toFixed(2);
+        business.level++;
+        const upgradeCost = Math.pow(type.upgradeRate, business.level) * type.purchase;
+        business.profit = type.profit * business.level;
+        business.timestamp = new Date().getTime();
+        business.upgradeCost = upgradeCost.toFixed(2);
 
-      // Update business with new level.
-      BusinessCollection.update({ _id: businessId }, business);
+        // Update business with new level.
+        BusinessCollection.update({ _id: businessId }, business);
+      }
     }
   },
 
@@ -61,7 +65,6 @@ Meteor.methods({
 
     // Deposit business profit.
     const cash = +(player.cash + business.profit);
-    console.log(player.cash, business.profit, cash);
     player.cash = +cash.toFixed(2);
     player.updatedAt = new Date().getTime();
     PlayersCollection.upsert({ _id: playerId }, player);
